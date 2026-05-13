49892e595989ee904be05dfd5abfd29b3ffd53a5
Anastasija <anastasija.lukarova@student.um.si>
2026-05-13 23:18:04 +0200

    SCRUM-17 Implement unstuffing and packet parsing

--- parser.py
+++ parser.py
@@ -22,3 +22,62 @@ def crc16_compute(data):
     for b in data:
         crc = crc16_update(crc, b)
     return crc
+
+def unstuff_bytes(data):
+    result = bytearray()
+    i = 0
+
+    while i < len(data):
+        if data[i] == 0xFE:
+            if i + 1 >= len(data):
+                break
+            result.append(0xFE ^ data[i + 1])
+            i += 2
+        else:
+            result.append(data[i])
+            i += 1
+
+    return bytes(result)
+
+def parse_packet(raw_packet):
+    payload = unstuff_bytes(raw_packet[1:])
+
+    if len(payload) < 8:
+        return None
+
+    timestamp = struct.unpack('<I', payload[0:4])[0]
+
+    crc_recv = struct.unpack('<H', payload[-2:])[0]
+    crc_calc = crc16_compute(payload[:-2])
+
+    if crc_recv != crc_calc:
+        return None
+
+    pos = 6
+    end = len(payload) - 2
+
+    paketi = []
+
+    while pos < end:
+        cid = payload[pos]
+        size = struct.unpack('<H', payload[pos+1:pos+3])[0] + 1
+
+        start = pos + 4
+        stop = start + size
+
+        chunk = payload[start:stop]
+
+        samples = []
+        for i in range(0, len(chunk), 6):
+            if i + 6 > len(chunk):
+                break
+            x, y, z = struct.unpack('<hhh', chunk[i:i+6])
+            samples.append((x, y, z))
+
+        data = np.array(samples, dtype=np.int16)
+
+        paketi.append(Paket(cid, timestamp / 1000.0, data))
+        pos = stop
+
+    return paketi
+
