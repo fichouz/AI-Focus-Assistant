39eae70c092fc35ccc8a616ea04dc4795c3233a9
Anastasija <anastasija.lukarova@student.um.si>
2026-05-14 20:35:54 +0200

    SCRUM-32 Implement serial data reading and validation

--- live_plotting.py
+++ live_plotting.py
@@ -1,8 +1,12 @@
+import serial
+import time
+import threading
 import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
+from parser import extract_packets, parse_packet
 from analysis import sestavi_podatke
 
 PORT = "COM7"
@@ -12,6 +16,38 @@ FILENAME = "sample.bin"
 buffer = b""
 paketi = []
 running = True
+
+def read_serial():
+    global buffer, paketi
+
+    try:
+        ser = serial.Serial(PORT, BAUD, timeout=1)
+        print(f"[OK] Connected to {PORT}")
+    except Exception as e:
+        print("[ERROR] Serial failed:", e)
+        return
+
+    with open(FILENAME, "wb") as f:
+        while running:
+            data = ser.read(256)
+
+            if data:
+                print(f"[DATA] {len(data)} bytes")
+
+                f.write(data)
+
+                buffer += data
+
+                raw_packets = extract_packets(buffer)
+
+                if raw_packets:
+                    last_packet = raw_packets[-1]
+                    last_index = buffer.rfind(last_packet)
+                    buffer = buffer[last_index + len(last_packet):]
+
+                for rp in raw_packets:
+                    parsed = parse_packet(rp)
+                    paketi.extend(parsed)
 
 def live_plot():
     print("[OK] Starting plot window...")
@@ -54,3 +90,22 @@ def live_plot():
 
         plt.tight_layout()
         plt.pause(0.05)
+
+
+if __name__ == "__main__":
+    print("[START] Program running...")
+
+    t1 = threading.Thread(target=read_serial)
+    t2 = threading.Thread(target=live_plot)
+
+    t1.start()
+    t2.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\n[STOP] Closing...")
+        running = False
+        t1.join()
+        t2.join()
